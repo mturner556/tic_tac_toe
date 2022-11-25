9ed1f0de7da55257afcd6cf3bdf9cdc2645c8509
Matt Turner <mattturner1911@gmail.com>
2022-11-25 16:21:48 -0500

    Created a Game class, with methods to create and set the players, and a method to show the display the board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -15,9 +15,41 @@ end
 
 # Player class
 class Player
-  attr_reader :name
+  attr_reader :name, :mark
 
-  def initialize(name)
+  def initialize(name, mark)
     @name = name
+    @mark = mark
   end
 end
+
+# Game class. Contains the methods for creating players and setting the board
+class Game
+  attr_reader :player_one, :player_two
+  
+  def initialize
+    @player_one = nil
+    @player_two = nil
+    @current_player = nil
+    @game_board = Board.new
+  end
+
+  def create_player(number)
+    puts "Player #{number}, what is your name?"
+    name = gets.chomp
+    puts "#{name}, what would you like your mark to be?" 
+    mark = gets.chomp
+    Player.new(name, mark)
+  end
+
+  def set_players
+    @player_one = create_player(1)
+    @player_two = create_player(2)
+  end
+
+  def show_board
+    @game_board.display_board()
+  end
+end
+
+new_game = Game.new
