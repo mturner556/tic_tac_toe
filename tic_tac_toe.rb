0f578fee92dc5c68e867285ab004d501302449de
Matt Turner <mattturner1911@gmail.com>
2022-11-28 07:35:21 -0500

    Added variable winning_combos that store the winningindicies. Created a check_winner method in the Game class. Check_winner does work, but will need to be adjusted to stop the game when a winner puts their last mark on the board.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -8,6 +8,21 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+# Stores the winning combos
+winning_combos = [
+  # rows
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  # columns
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  # diagonal
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 # Player class
 class Player
   attr_reader :name, :mark
@@ -53,16 +68,28 @@ class Game
   def turn
     @current_player = @current_player == player_one ? player_two : player_one
   end
+
+  # checks for a winner
+  def check_winner(board, combo)
+    # if the combo is filled with the same player mark, puts winner and end the game
+    combo.each do |e1, e2, e3|
+      if board[e1] == @current_player.mark && board[e2] == @current_player.mark && board[e3] == @current_player.mark
+        puts "#{@current_player.name} you're have won the game!"
+      end
+    end
+  end
 end
 
 # calling and testing methods from the Game class
 new_game = Game.new
 
+# Plays the game 9 times
 i = 0
 
 while i < 9
   display_board(board)
   new_game.player_selection(board)
   new_game.turn
+  new_game.check_winner(board, winning_combos)
   i += 1
 end
